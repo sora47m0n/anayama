9c9ff29fb47e5317a75ed11e53469d67faa9d634
Sora Ashizawa <g023c1022@g.neec.ac.jp>
2026-01-14 16:45:17 +0900

    test

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>変更しました</h1>
+    <main style={{ padding: "40px", fontSize: "24px" }}>
+      <h1>こんにちは 👋</h1>
+      <p>anayama フロントエンドが正常に動いています。</p>
+    </main>
   );
 }
